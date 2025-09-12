6ec56167d5d67ab087116280e1ebcac4dd6fd96d
YazminGalvez <L22440144@mochis.tecnm.mx>
2025-09-11 18:18:36 -0700

    modifique para que el cliente mande mensaje al servidor

--- src/cliente.java
+++ src/cliente.java
@@ -1,4 +1,8 @@
 import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.*;
 import java.util.Scanner;
 
